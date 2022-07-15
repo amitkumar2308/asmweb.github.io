1df813d3577f4bfc85202cc1cf9c22550a242631
AMIT KUMAR <97108600+amitkumar2308@users.noreply.github.com>
2022-07-15 08:25:51 +0530

    initial commit

---
+++ asmscroll.js
@@ -0,0 +1,40 @@
+function feature() {
+    var reveals = document.querySelectorAll(".feature");
+  
+    for (var i = 0; i < reveals.length; i++) {
+      var windowHeight = window.innerHeight;
+      var elementTop = reveals[i].getBoundingClientRect().top;
+      var elementVisible = 150;
+  
+      if (elementTop < windowHeight - elementVisible) {
+        reveals[i].classList.add("active");
+      } else {
+        reveals[i].classList.remove("active");
+      }
+    }
+  }
+  
+  window.addEventListener("scroll", feature);
+
+
+  function feature2() {
+    var reveals = document.querySelectorAll(".feature2");
+  
+    for (var i = 0; i < reveals.length; i++) {
+      var windowHeight = window.innerHeight;
+      var elementTop = reveals[i].getBoundingClientRect().top;
+      var elementVisible = 190;
+  
+      if (elementTop < windowHeight - elementVisible) {
+        reveals[i].classList.add("active");
+      } else {
+        reveals[i].classList.remove("active");
+      }
+    }
+  }
+  
+  window.addEventListener("scroll", feature2);
+
+
+ 
+
